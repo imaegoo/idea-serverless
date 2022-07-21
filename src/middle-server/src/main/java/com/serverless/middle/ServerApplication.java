7b04a082fd641753aaa489f0babd116f9ee2d0c1
tuwenlin <t529791649@163.com>
2022-07-21 19:57:39 +0800

    pom

--- src/middle-server/src/main/java/com/serverless/middle/ServerApplication.java
+++ src/middle-server/src/main/java/com/serverless/middle/ServerApplication.java
@@ -1,11 +1,12 @@
 package com.serverless.middle;
 
 import com.serverless.middle.server.Server;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ServerApplication {
 
-    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(ServerApplication.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServerApplication.class);
 
     public static void main(String[] args) {
         long beginTime = System.currentTimeMillis();
